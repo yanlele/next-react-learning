cd68b5baee063088a20be3d7bdc37f5ae0265cd1
yanlele <331393627@qq.com>
2018-12-30 14:35:14 +0800

    [+]提交实例文档

---
+++ pages/home/home.js
@@ -0,0 +1,15 @@
+/**
+ * create by yanle
+ * connect me 331393627@qq.com
+ * create time 2018-12-30 14:27
+ */
+
+function Main() {
+    return (
+        <div>
+            <img src='/static/img/01.png' alt=""/>
+        </div>
+    )
+}
+
+export default Main;
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+/**
+ * create by yanle
+ * connect me 331393627@qq.com
+ * create time 2018-12-30 14:17
+ */
+import '../style/index.less';
+
+function Index() {
+    let str = 'yanlele';
+
+    return (
+        <div>
+            <p>hello 123</p>
+            <p className="test">{str}</p>
+        </div>
+    )
+}
+
+export default Index;
